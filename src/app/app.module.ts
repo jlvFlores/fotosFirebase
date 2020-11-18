b5b160e83c0bcf53f70178db072c1f66e75bff0d
Jose Luis <jluisvazquez.jv@gmail.com>
2020-11-18 17:54:08 -0600

    315 componentes y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, APP_ROUTES } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotosComponent,
+    CargaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
